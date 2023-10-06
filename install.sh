09f191cd7f65b2b1e3eb5a7f135e34a63d53d15d
MEGA_Dawg68 <140035326+MEGA-Dawg68@users.noreply.github.com>
2023-10-06 22:07:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 # Download the HTML file from GitHub
 curl -o index.html https://raw.githubusercontent.com/<MEGA-Dawg68>/<Starter-Page>/<main>/search.html
 
-# Replace the Firefox start page with the downloaded HTML file
+# Replace the Firefox start page with the my start-page file
 sed -i 's/browser.startup.homepage=.*/browser.startup.homepage=file:\/\/\/path\/to\/index.html/g' ~/.mozilla/firefox/*.default/prefs.js
